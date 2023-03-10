e217925737e3589f75d6d2124f5e2a9bee7a11c3
thirumoorthy2005 <thirumoorthy@capeindia.net>
2023-03-10 18:05:49 +0530

    api gatway configuration changes

--- src/main/java/com/capeelectric/ApiGatewayApplication.java
+++ src/main/java/com/capeelectric/ApiGatewayApplication.java
@@ -41,19 +41,19 @@ public class ApiGatewayApplication {
 //		return new CorsWebFilter(source);
 //	}
 	
-	 @Bean
-	    public CorsWebFilter corsWebFilter() {
-
-	        final CorsConfiguration corsConfig = new CorsConfiguration();
-	        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
-	        corsConfig.setMaxAge(3600L);
-	        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
-	        corsConfig.addAllowedHeader("*");
-
-	        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", corsConfig);
-
-	        return new CorsWebFilter(source);
-	    } 
+//	 @Bean
+//	    public CorsWebFilter corsWebFilter() {
+//
+//	        final CorsConfiguration corsConfig = new CorsConfiguration();
+//	        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+//	        corsConfig.setMaxAge(3600L);
+//	        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+//	        corsConfig.addAllowedHeader("*");
+//
+//	        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	        source.registerCorsConfiguration("/**", corsConfig);
+//
+//	        return new CorsWebFilter(source);
+//	    } 
  
 }
